bf439bf0cdb994f7dbeedb9f214a69803c01ec0d
Fenosoa <fenosoarp6@gmail.com>
2024-02-15 07:48:34 +0300

    last commit

--- app.js
+++ app.js
@@ -10,16 +10,6 @@ const UtilisateurResource = require("./resources/UtilisateurResource");
 
 require("./config/passport");
 
-// const verifier_token = (req , res , next ) =>{
-//     const authHeader = req.headers['authorization'];
-//     const token = authHeader && authHeader.split(' ')[1]; // Extraction du token JWT depuis l'en-tête Authorization
-
-//     if (!token) {
-//         return res.status(401).json({ message: 'Merci de vous connecter.' }); // Token non fourni, renvoie un message personnalisé
-//     }
-//     next()
-// }
-
 const app = express();
 
 app.use(fileUpload());
@@ -29,12 +19,18 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ extended: false }));
 
+const verification_authentification = (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (err || !user) {
+      return res.status(401).json({ message: "Merci de vous connecter" });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
+
 app.use("/api", SecurityResource);
-app.use(
-  "/api/utilisateur",
-  passport.authenticate("jwt", { session: false }),
-  UtilisateurResource
-);
+app.use("/api/utilisateur", verification_authentification, UtilisateurResource);
 
 db.sequelize.sync().then(() => {
   app.listen(9000, () => {
